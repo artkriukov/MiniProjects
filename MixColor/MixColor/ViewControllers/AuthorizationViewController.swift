b6fcdffe32b2a33195fb428cb7c7fcc8a85ed0c7
Artem Kriukov <art.kriukov@gmail.com>
2024-12-24 19:15:52 +0300

    add AuthorizationVC and rename MainVC -> ColorPickerVC

---
+++ MixColor/MixColor/ViewControllers/AuthorizationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AuthorizationViewController.swift
+//  MixColor
+//
+//  Created by Artem on 12/24/24.
+//
+
+import UIKit
+
+class AuthorizationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MixColor/MixColor/ViewControllers/ColorPickerViewController.swift
+++ MixColor/MixColor/ViewControllers/ColorPickerViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class ColorPickerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
